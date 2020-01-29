ff1a557aeeadcfb443f35151dce6fc2af02ce91b
sasha walker <sashawalker31@gmail.com>
2020-01-29 15:08:39 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, "Ada")
+  take_a_number(katz_deli, "Grace")
+  take_a_number(katz_deli, "Kent")
+  line(katz_deli)
+  now_serving(katz_deli)
+  line(katz_deli)
+  take_a_number(katz_deli, "Matz")
+  line(katz_deli)
+  now_serving(katz_deli)
+  line(katz_deli)
+end
+end
