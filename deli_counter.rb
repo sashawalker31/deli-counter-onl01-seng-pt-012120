492b3570a7a567004d94f353fe55f627be88ba8c
sasha walker <sashawalker31@gmail.com>
2020-01-29 14:48:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli=[])
   if katz_deli.length ==0
-    puts "line is empty."
+    puts "The line is currently empty."
   elsif katz_deli.length == 3 
     puts "The line is currently: #{name} #{number}"
   end
