2610150ba4b7fbb3291fc1c07d459aa500e26c59
sasha walker <sashawalker31@gmail.com>
2020-01-29 15:08:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,13 @@
 katz_deli = []
 
-take_a_number(katz_deli, "Ada")
-take_a_number(katz_deli, "Grace")
-take_a_number(katz_deli, "Kent")
+def take_a_number(katz_deli, "Ada")
+  take_a_number(katz_deli, "Grace")
+  take_a_number(katz_deli, "Kent")
+  line(katz_deli)
+  now_serving(katz_deli)
+  line(katz_deli)
+  take_a_number(katz_deli, "Matz")
 line(katz_deli)
 now_serving(katz_deli)
 line(katz_deli)
-take_a_number(katz_deli, "Matz")
-line(katz_deli)
-now_serving(katz_deli)
-line(katz_deli)
+end
